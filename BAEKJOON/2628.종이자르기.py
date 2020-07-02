6fa8c13599948a2212a38aaa9701e42cb0350b23
kyngs_kwk <kyngskwk@gmail.com>
2020-07-02 22:48:28 +0900

    2628

---
+++ BAEKJOON/2628.종이자르기.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.stdin = open('input.txt', 'r')
+
+x, y = map(int, input().split())
+A = [0] + [x]
+B = [0] + [y]
+for tc in range(int(input())):
+    K, P = map(int, input().split())
+    if K == 0:
+        B += [P]
+    else:
+        A += [P]
+
+A_max, B_max = 0, 0
+A, B = sorted(A), sorted(B)
+
+for idx in range(1, len(A)):
+    A_max = max(A_max, A[idx] - A[idx-1])
+
+for idx in range(1, len(B)):
+    B_max = max(B_max, B[idx] - B[idx-1])
+
+print(A_max * B_max)
