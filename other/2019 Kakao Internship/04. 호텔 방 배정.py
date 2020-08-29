a5c8d5c60f2a69cba548f336b555f93993c113ec
kyngs_kwk <kyngskwk@gmail.com>
2020-08-30 02:38:05 +0900

    0830 kakao algo 4

--- other/2019 Kakao Internship/04. 호텔 방 배정.py
+++ other/2019 Kakao Internship/04. 호텔 방 배정.py
@@ -12,27 +12,43 @@ def solution(k, room_number):
     return answer
 
 # 효율성...............
-hash = {}
-def findroom(idx):
-    global hash
-    idx += 1
-    if idx not in hash:
-        hash[idx] = 1
-        return
-    else:
-        findroom(idx)
-
 def solution(k, room_number):
-    global hash
+    room_dict = {}
     answer = []
     for num in room_number:
-        if num not in hash:
-            hash[num] = 1
-        else:
-            findroom(num)
+        n = num
+        visit = [n]
+        while n in room_dict:
+            n = room_dict[n]
+            visit.append(n)
+        answer.append(n)
+        for v in visit:
+            room_dict[v] = n+1
+    return answer
 
-    for key in hash.keys():
-        answer.append(key)
+# 다른 코드
+def solution(k, room_number):
+    answer = []
+    room = {}
+    for num in room_number:
+        print(num)
+        number = room.get(num, 0)
+        if number != 0:
+            temp = [num]
+            while True:
+                index = number
+                number = room.get(number, 0)
+                print(number, room)
+                if number == 0:
+                    answer.append(index)
+                    room[index] = index + 1
+                    for i in temp:
+                        room[i] = index + 1
+                    break
+                temp.append(number)
+        else:
+            answer.append(num)
+            room[num] = num + 1
     return answer
 
 print(solution(10, [1,3,4,1,3,1]))
