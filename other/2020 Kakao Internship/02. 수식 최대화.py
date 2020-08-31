d08e36cd8ca640f8d7de0ed9295901a776f4078d
kyngs_kwk <kyngskwk@gmail.com>
2020-08-31 23:23:44 +0900

    0831 kakao algo 2 yet

--- other/2020 Kakao Internship/02. 수식 최대화.py
+++ other/2020 Kakao Internship/02. 수식 최대화.py
@@ -1,52 +1,82 @@
-
-
-def perm(nums, k, N, answer, sign_list):
-    if k == N:
-        sum = nums[0]
-        for idx in range(len(sign_list)):
-            print(idx)
-            if sign_list[idx] == '-':
-                sum -= nums[idx+1]
-            elif sign_list[idx] == '+':
-                sum += nums[idx+1]
-            else:
-                sum *= nums[idx+1]
-        print(sum)
-        answer = max(sum, answer)
-        print(answer)
-
-    else:
-        for i in range(k, N):
-            nums[k], nums[i] = nums[i], nums[k]
-            perm(nums, k + 1, N, answer, sign_list)
-            nums[k], nums[i] = nums[i], nums[k]
-
+from itertools import permutations
 
 def solution(expression):
-    input = list(expression)
     sign = ['-', '+', '*']
     nums = []
-    num = ''
-    sign_list = []
-    long = len(input)
+    signset = []
+    start = 0
+    for idx in range(len(expression)):
+        if expression[idx] in sign:
+            nums.append(int(expression[start:idx]))
+            signset.append(expression[idx])
+            start = idx+1
+    nums.append(int(expression[start:]))
 
-    answer = 0
+    my_max = 0
+    nums = list(permutations(nums, len(nums)))
+    for numset in nums:
+        my_sum = numset[0]
+        for idx in range(len(signset)):
+            if signset[idx] == '*':
+                my_sum *= numset[idx+1]
+            elif signset[idx] == '+':
+                my_sum += numset[idx+1]
+            else:
+                my_sum -= numset[idx+1]
+        my_max = max(my_max, abs(my_sum))
 
-    for idx in range(long):
-        now = input.pop(0)
-        if now in sign:
-            nums.append(num)
-            sign_list.append(now)
-            num = ''
-        elif len(input) == 0:
-            num += now
-            nums.append(num)
-            num = ' '
-        else:
-            num += now
-    print(sign_list)
-    nums = list(map(int, nums))
-    perm(nums, 0, len(nums), answer, sign_list)
-    return answer
+    return my_max
+
+
+#
+#
+# def perm(nums, k, N, answer, sign_list):
+#     if k == N:
+#         sum = nums[0]
+#         for idx in range(len(sign_list)):
+#             print(idx)
+#             if sign_list[idx] == '-':
+#                 sum -= nums[idx+1]
+#             elif sign_list[idx] == '+':
+#                 sum += nums[idx+1]
+#             else:
+#                 sum *= nums[idx+1]
+#         print(sum)
+#         answer = max(sum, answer)
+#         print(answer)
+#
+#     else:
+#         for i in range(k, N):
+#             nums[k], nums[i] = nums[i], nums[k]
+#             perm(nums, k + 1, N, answer, sign_list)
+#             nums[k], nums[i] = nums[i], nums[k]
+#
+#
+# def solution(expression):
+#     input = list(expression)
+#     sign = ['-', '+', '*']
+#     nums = []
+#     num = ''
+#     sign_list = []
+#     long = len(input)
+#
+#     answer = 0
+#
+#     for idx in range(long):
+#         now = input.pop(0)
+#         if now in sign:
+#             nums.append(num)
+#             sign_list.append(now)
+#             num = ''
+#         elif len(input) == 0:
+#             num += now
+#             nums.append(num)
+#             num = ' '
+#         else:
+#             num += now
+#     print(sign_list)
+#     nums = list(map(int, nums))
+#     perm(nums, 0, len(nums), answer, sign_list)
+#     return answer
 
 print(solution("50*6-3*2"))
