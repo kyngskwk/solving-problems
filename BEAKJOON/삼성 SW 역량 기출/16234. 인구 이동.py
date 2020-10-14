5db0eca453da93be6f771f0dd0357cb8a61b83e6
kyngs_kwk <kyngskwk@gmail.com>
2020-10-14 11:49:12 +0900

    1014 beakjoon 16234 re

--- BEAKJOON/삼성 SW 역량 기출/16234. 인구 이동.py
+++ BEAKJOON/삼성 SW 역량 기출/16234. 인구 이동.py
@@ -5,11 +5,17 @@ sys.stdin = open("input.txt", "r")
 cnt = 0
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-temp = []
 
 def find(pan, i, j, v):
     global temp
-    if
+    for idx in range(4):
+        n_x = i + dx[idx]
+        n_y = j + dy[idx]
+        if L <= abs(pan[i][j] - pan[n_x][n_y]) <= R and pan[n_x][n_y] >= 0:
+            if v[n_x][n_y] == 0:
+                v[n_x][n_y] = 1
+                temp.append([n_x, n_y])
+                find(pan, n_x, n_y, v)
 
 
 N, L, R = map(int, input().split())
@@ -17,46 +23,30 @@ pan = [[-1] * (N+2) for _ in range(N+2)]
 for idx in range(1, N+1):
     pan[idx][1:N+1] = list(map(int, input().split()))
 
-v = [[0] * (N+2) for _ in range(N+2)]
+print(pan)
+temp = []
+while True:
+    v = [[0] * (N + 2) for _ in range(N + 2)]
+    flag = 0
+    for i in range(1 + N+1):
+        for j in range(1 + N+1):
+            if v[i][j] == 0 and pan[i][j] >= 0:
+                v[i][j] = 1
+                temp = [[i, j]]
+                find(pan, i, j, v)
 
+            if len(temp) > 1:
+                flag = 1
+                pp = 0
+                for t in temp:
+                    pp += pan[t[0]][t[1]]
+                for k in temp:
+                    pan[k[0]][k[1]] = pp // len(temp)
 
-
-# def gap(pan, i, j, v, L, R):
-#     global cnt, temp, pp
-#
-#     for idx in range(4):
-#         pgap = abs(pan[i+dx[idx]][j+dy[idx]] - pan[i][j])
-#         if pgap >= L and pgap <= R and v[i+dx[idx]][j+dy[idx]] == 0 and pan[i+dx[idx]][j+dy[idx]] >= 0:
-#             v[i + dx[idx]][j + dy[idx]] = 1
-#             gap(pan, i+dx[idx], j+dy[idx], v, L, R)
-#             pp += pan[i+dx[idx]][j+dy[idx]]
-#             temp.append((i+dx[idx], j+dy[idx]))
-#
-#
-# N, L, R = map(int, input().split())
-# pan = [[-1] * (N+2) for _ in range(N+2)]
-# for idx in range(1, N+1):
-#     pan[idx][1:N+1] = list(map(int, input().split()))
-#
-#
-# v = [[0] * (N+2) for _ in range(N+2)]
-# while True:
-#     for i in range(1, N+1):
-#         for j in range(1, N+1):
-#             if v[i][j] == 0:
-#                 pp = pan[i][j]
-#                 temp = [(i, j)]
-#                 gap(pan, i, j, v, L, R)
-#                 if len(temp) > 1:
-#                     cnt += 1
-#                     print(temp)
-#                     change = pp // len(temp)
-#                     for p, q in temp:
-#                         pan[p][q] = change
-#
-#     if len(temp) == 1:
-#         break
-
+    if flag == 0:
+        break
+    else:
+        cnt += 1
 
 
 print(cnt)
